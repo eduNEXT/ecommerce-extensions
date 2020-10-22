d5223ad1738a02308d209e766f73a1fe895d04a3
Andrey Canon <andrey.canon@edunext.co>
2020-10-22 16:49:52 -0500

    rename label

--- ecommerce_extensions/tenant/apps.py
+++ ecommerce_extensions/tenant/apps.py
@@ -11,4 +11,5 @@ class TenantConfig(AppConfig):
     Django eduNEXT e-commerce tenant configuration.
     """
     name = 'ecommerce_extensions.tenant'
+    label = 'edunext'
     verbose_name = 'eduNEXT tenants'

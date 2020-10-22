3a0025e7280e9a0ccdd223aeb891b4fbb569e447
Andrey Canon <andrey.canon@edunext.co>
2020-10-22 15:04:01 -0500

    Override setting by signals.

--- eco_tenant/apps.py
+++ eco_tenant/apps.py
@@ -1,10 +1,10 @@
 """
 App configuration for eco_tenant.
 """
-
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.core.signals import request_started
 
 
 class EcoTenantConfig(AppConfig):
@@ -14,3 +14,10 @@ class EcoTenantConfig(AppConfig):
     name = 'eco_tenant'
     label = 'edunext'
     verbose_name = 'eduNEXT tenant'
+
+    def ready(self):
+        """
+        Method to perform actions after apps registry is ended
+        """
+        from eco_tenant.receivers import update_tenant_settings
+        request_started.connect(update_tenant_settings)
--- eco_tenant/conf.py
+++ eco_tenant/conf.py
@@ -11,40 +11,47 @@ from ecommerce.edunext.conf import settings
 """
 import logging
 
-from django.conf import settings as base_settings
-from threadlocals.threadlocals import get_current_request
+from django.contrib.sites.models import Site
 
 from eco_tenant.models import TenantOptions
 
 LOG = logging.getLogger(__name__)
 
 
-class SiteAwareSettings(object):
+class TenantAwareSettings(object):
     """
     This class is a proxy object of the settings object from django.
     It will try to get a value from the microsite and default to the
     django settings
     """
 
-    def get_current_request_site_options(self):
+    @classmethod
+    def get_site_by_domain(cls, domain):
+        """
+        Returns the associated site for the given domain.
+
+        Arguments:
+            domain: String.
+
+        Return:
+            django.contrib.sites.models.Site instance.
+        """
+        try:
+            return Site.objects.get(domain=domain)
+        except Site.DoesNotExist:  # pylint: disable=no-member
+            return None
+
+    @classmethod
+    def get_tenant_options(cls, domain):
         """
         This method retrieves the edunext options for current request site
         """
-        site = get_current_request().site
+        site = cls.get_site_by_domain(domain)
+
         try:
-            options = site.tenantoptions.options_blob
+            options = site.tenantoptions.options_blob if site else {}
         except TenantOptions.DoesNotExist:  # pylint: disable=no-member
             LOG.warning("The site %s has no associated TenantOptions register.", site.domain)
             options = {}
 
         return options
-
-    def __getattr__(self, name):
-        current_site_options = self.get_current_request_site_options()
-        try:
-            return current_site_options.get(name, getattr(base_settings, name))
-        except KeyError:
-            return getattr(base_settings, name)
-
-
-settings = SiteAwareSettings()  # pylint: disable=invalid-name
